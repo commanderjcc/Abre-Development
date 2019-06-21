62cb10e0f253d48487b49cb043216cb0cfe769e9
Josh <josh.christensen48@masonohioschools.com>
2019-06-21 10:32:12 -0400

    Overhall installer.php

--- modules/Abre-Moods/installer.php
+++ modules/Abre-Moods/installer.php
@@ -20,9 +20,10 @@
 	require_once(dirname(__FILE__) . '/../../core/abre_verification.php');
 	require_once(dirname(__FILE__) . '/../../core/abre_functions.php');
 	//findme
-	if(superadmin() && !isAppInstalled("Abre-Moods"))
+	if(superadmin() && !isAppInstalled("MCS-Moods"))
 	{
 
+		/* Left just for reference
 		//Create Books folder if one does not exist
 		if (!file_exists("$portal_path_root/content/books")){ mkdir("$portal_path_root/content/books", 0700); }
 
@@ -31,149 +32,64 @@
 			if (!mkdir("$portal_path_root/../$portal_private_root/books", 0775)) {
 			}
 		}
+		*/
 
-		//Check for books table
+
+		//###################### DATABASE STRUCTURE ###########################
+		//    ID      |  user_ID   |  last_mood  |  mood_history  |  siteID
+		// -------------------------------------------------------------------
+ 		//   int(11)  |  int(11)   | text(JSON)  |   text(JSON)   |  int(11)
+
+		//Check for Moods table
 		require(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
-		if(!$db->query("SELECT * FROM books LIMIT 1"))
+		if(!$db->query("SELECT * FROM moods LIMIT 1"))
 		{
-			$sql = "CREATE TABLE `books` (`ID` int(11) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
-			$sql .= "ALTER TABLE `books` ADD PRIMARY KEY (`ID`);";
-			$sql .= "ALTER TABLE `books` MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;";
+			$sql = "CREATE TABLE `moods` (`ID` int(11) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+			$sql .= "ALTER TABLE `moods` ADD PRIMARY KEY (`ID`);";
+			$sql .= "ALTER TABLE `moods` MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;";
 			$db->multi_query($sql);
 		}
 		$db->close();
 
-		//Check for Code field
+		//Check for user_ID field
 		require(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
-		if(!$db->query("SELECT Code FROM books LIMIT 1"))
+		if(!$db->query("SELECT user_ID FROM moods LIMIT 1"))
 		{
-			$sql = "ALTER TABLE `books` ADD `Code` varchar(11) NOT NULL;";
+			$sql = "ALTER TABLE `moods` ADD `user_ID` varchar(11) NOT NULL;";
 			$db->multi_query($sql);
 		}
 		$db->close();
 
-		//Check for Code field
+		//Check for last_mood field
 		require(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
-		if(!$db->query("SELECT Code FROM books LIMIT 1"))
+		if(!$db->query("SELECT last_mood FROM moods LIMIT 1"))
 		{
-			$sql = "ALTER TABLE `books` ADD `Code` varchar(11) NOT NULL;";
+			$sql = "ALTER TABLE `moods` ADD `last_mood` text NOT NULL;";
 			$db->multi_query($sql);
 		}
 		$db->close();
 
-		//Check for Code_Limit field
+		//Check for mood_history field
 		require(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
-		if(!$db->query("SELECT Code_Limit FROM books LIMIT 1"))
+		if(!$db->query("SELECT mood_history FROM moods LIMIT 1"))
 		{
-			$sql = "ALTER TABLE `books` ADD `Code_Limit` int(11) DEFAULT NULL;";
+			$sql = "ALTER TABLE `moods` ADD `mood_history` text NOT NULL;";
 			$db->multi_query($sql);
 		}
 		$db->close();
 
-		//Check for Title field
+		//Check for siteID field
 		require(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
-		if(!$db->query("SELECT Title FROM books LIMIT 1"))
+		if(!$db->query("SELECT siteID FROM moods LIMIT 1"))
 		{
-			$sql = "ALTER TABLE `books` ADD `Title` text NOT NULL;";
-			$db->multi_query($sql);
-		}
-		$db->close();
-
-		//Check for Author field
-		require(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
-		if(!$db->query("SELECT Author FROM books LIMIT 1"))
-		{
-			$sql = "ALTER TABLE `books` ADD `Author` text NOT NULL;";
-			$db->multi_query($sql);
-		}
-		$db->close();
-
-		//Check for Slug field
-		require(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
-		if(!$db->query("SELECT Slug FROM books LIMIT 1"))
-		{
-			$sql = "ALTER TABLE `books` ADD `Slug` text NOT NULL;";
-			$db->multi_query($sql);
-		}
-		$db->close();
-
-		//Check for Cover field
-		require(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
-		if(!$db->query("SELECT Cover FROM books LIMIT 1"))
-		{
-			$sql = "ALTER TABLE `books` ADD `Cover` text NOT NULL;";
-			$db->multi_query($sql);
-		}
-		$db->close();
-
-		//Check for File field
-		require(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
-		if(!$db->query("SELECT File FROM books LIMIT 1"))
-		{
-			$sql = "ALTER TABLE `books` ADD `File` text NOT NULL;";
-			$db->multi_query($sql);
-		}
-		$db->close();
-
-		//Check for Students_Required field
-		require(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
-		if(!$db->query("SELECT Students_Required FROM books LIMIT 1"))
-		{
-			$sql = "ALTER TABLE `books` ADD `Students_Required` int(11) NOT NULL DEFAULT '0';";
-			$db->multi_query($sql);
-		}
-		$db->close();
-
-		//Check for Staff_Required field
-		require(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
-		if(!$db->query("SELECT Staff_Required FROM books LIMIT 1"))
-		{
-			$sql = "ALTER TABLE `books` ADD `Staff_Required` int(11) NOT NULL DEFAULT '0';";
-			$db->multi_query($sql);
-		}
-		$db->close();
-
-		//Check for books_libraries table
-		require(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
-		if(!$db->query("SELECT * FROM books_libraries LIMIT 1"))
-		{
-			$sql = "CREATE TABLE `books_libraries` (`ID` int(11) NOT NULL) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;";
-			$sql .= "ALTER TABLE `books_libraries` ADD PRIMARY KEY (`ID`);";
-			$sql .= "ALTER TABLE `books_libraries` MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;";
-			$db->multi_query($sql);
-		}
-		$db->close();
-
-		//Check for User_ID field
-		require(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
-		if(!$db->query("SELECT User_ID FROM books_libraries LIMIT 1"))
-		{
-			$sql = "ALTER TABLE `books_libraries` ADD `User_ID` int(11) NOT NULL;";
-			$db->multi_query($sql);
-		}
-		$db->close();
-
-		//Check for Parent_ID field
-		require(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
-		if(!$db->query("SELECT Parent_ID FROM books_libraries LIMIT 1"))
-		{
-			$sql = "ALTER TABLE `books_libraries` ADD `Parent_ID` int(11) NOT NULL;";
-			$db->multi_query($sql);
-		}
-		$db->close();
-
-		//Check for Book_ID field
-		require(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
-		if(!$db->query("SELECT Book_ID FROM books_libraries LIMIT 1"))
-		{
-			$sql = "ALTER TABLE `books_libraries` ADD `Book_ID` int(11) NOT NULL;";
+			$sql = "ALTER TABLE `moods` ADD `siteID` int(11) NOT NULL;";
 			$db->multi_query($sql);
 		}
 		$db->close();
 
 		//Mark app as installed
 		require(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
-		$sql = "UPDATE apps_abre SET installed = 1 WHERE app = 'Abre-Books'";
+		$sql = "UPDATE apps_abre SET installed = 1 WHERE app = 'MCS-Moods'";
 		$db->multi_query($sql);
 		$db->close();
 
