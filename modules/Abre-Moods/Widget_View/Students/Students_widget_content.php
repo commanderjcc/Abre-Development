37725c1d886706df54d26705c60aa3eb44efdcd5
Josh <Josh.christensen48@masonohioschools.com>
2019-06-27 11:06:11 -0400

    reformat html to look nice

--- modules/Abre-Moods/Widget_View/Students/Students_widget_content.php
+++ modules/Abre-Moods/Widget_View/Students/Students_widget_content.php
@@ -23,91 +23,91 @@ require_once(dirname(__FILE__) . '/../../../../core/abre_verification.php'); //r
 echo "<hr class='widget_hr'>"; //keep
 //widget layout
 echo "<div class='widget_emoji_container'>
-        <div class='widget_emoji_container emoji_row'>
-          <div id='widget_happy' class='widget_emoji'>
-            <i class='twa twa-5x twa-slightly-smiling-face'></i>
-            Happy
+          <div class='widget_emoji_container emoji_row'>
+              <div id='widget_happy' class='widget_emoji'>
+                  <i class='twa twa-5x twa-slightly-smiling-face'></i>
+                  Happy
+              </div>
+              <div id='widget_okay' class='widget_emoji'>
+                  <i class='twa twa-5x twa-neutral-face'></i>
+                  Okay
+              </div>
+              <div id='widget_down' class='widget_emoji'>
+                  <i class='twa twa-5x twa-confused-face'></i>
+                  Down
+              </div>
           </div>
-          <div id='widget_okay' class='widget_emoji'>
-            <i class='twa twa-5x twa-neutral-face'></i>
-            Okay
+          <div class='widget_emoji_container emoji_row'>
+              <div id='widget_thrilled' class='widget_emoji'>
+                  <i class='twa twa-5x twa-beaming-face-with-smiling-eyes'></i>
+                  Thrilled
+              </div>
+              <div id='widget_meh' class='widget_emoji'>
+                  <i class='twa twa-5x twa-expressionless-face'></i>
+                  Meh
+              </div>
+              <div id='widget_sad' class='widget_emoji'>
+                  <i class='twa twa-5x twa-loudly-crying-face'></i>
+                  Sad
+              </div>
           </div>
-          <div id='widget_down' class='widget_emoji'>
-            <i class='twa twa-5x twa-confused-face'></i>
-            Down
+          <div class='widget_emoji_container emoji_row'>
+              <div id='widget_stressed' class='widget_emoji'>
+                  <i class='twa twa-5x twa-grimacing-face'></i>
+                  Stressed
+              </div>
+              <div id='widget_worried' class='widget_emoji'>
+                  <i class='twa twa-5x twa-fearful-face'></i>
+                  Worried
+              </div>
+              <div id='widget_scared' class='widget_emoji'>
+                  <i class='twa twa-5x twa-face-screaming-in-fear'></i>
+                  Scared
+              </div>
           </div>
-        </div>
-        <div class='widget_emoji_container emoji_row'>
-          <div id='widget_thrilled' class='widget_emoji'>
-            <i class='twa twa-5x twa-beaming-face-with-smiling-eyes'></i>
-            Thrilled
+          <div class='widget_emoji_container emoji_row'>
+              <div id='widget_annoyed' class='widget_emoji'>
+                  <i class='twa twa-5x twa-unamused-face'></i>
+                  Annoyed
+              </div>
+              <div id='widget_angry' class='widget_emoji'>
+                  <i class='twa twa-5x twa-angry-face'></i>
+                  Angry
+              </div>
+              <div id='widget_frustrated' class='widget_emoji'>
+                  <i class='twa twa-5x twa-face-with-steam-from-nose'></i>
+                  Frustrated
+              </div>
           </div>
-          <div id='widget_meh' class='widget_emoji'>
-            <i class='twa twa-5x twa-expressionless-face'></i>
-            Meh
+          <div class='widget_emoji_container emoji_row'>
+              <div id='widget_allergic' class='widget_emoji'>
+                  <i class='twa twa-5x twa-sneezing-face'></i>
+                  Allergic
+              </div>
+              <div id='widget_sick' class='widget_emoji'>
+                  <i class='twa twa-5x twa-face-with-thermometer'></i>
+                  Sick
+              </div>
+              <div id='widget_tired' class='widget_emoji'>
+                  <i class='twa twa-5x twa-sleeping-face'></i>
+                  Tired
+              </div>
           </div>
-          <div id='widget_sad' class='widget_emoji'>
-            <i class='twa twa-5x twa-loudly-crying-face'></i>
-            Sad
+          <div class='widget_emoji_container emoji_row'>
+              <div id='widget_speak_up' class='widget_emoji'>
+                  <i class='twa twa-5x twa-eye-in-speech-bubble'></i>
+                  Speak Up
+              </div>
+              <div id='widget_needs_help' class='widget_emoji'>
+                  <i class='twa twa-5x twa-sos-button'></i>
+                  I need help
+              </div>
+              <div id='widget_needs_talk' class='widget_emoji'>
+                  <i class='twa twa-5x twa-speech-balloon'></i>
+                  I need to talk
+              </div>
           </div>
-        </div>
-        <div class='widget_emoji_container emoji_row'>
-          <div id='widget_stressed' class='widget_emoji'>
-            <i class='twa twa-5x twa-grimacing-face'></i>
-            Stressed
-          </div>
-          <div id='widget_worried' class='widget_emoji'>
-            <i class='twa twa-5x twa-fearful-face'></i>
-            Worried
-          </div>
-          <div id='widget_scared' class='widget_emoji'>
-            <i class='twa twa-5x twa-face-screaming-in-fear'></i>
-            Scared
-          </div>
-        </div>
-        <div class='widget_emoji_container emoji_row'>
-          <div id='widget_annoyed' class='widget_emoji'>
-            <i class='twa twa-5x twa-unamused-face'></i>
-            Annoyed
-          </div>
-          <div id='widget_angry' class='widget_emoji'>
-            <i class='twa twa-5x twa-angry-face'></i>
-            Angry
-          </div>
-          <div id='widget_frustrated' class='widget_emoji'>
-            <i class='twa twa-5x twa-face-with-steam-from-nose'></i>
-            Frustrated
-          </div>
-        </div>
-        <div class='widget_emoji_container emoji_row'>
-        <div id='widget_allergic' class='widget_emoji'>
-          <i class='twa twa-5x twa-sneezing-face'></i>
-          Allergic
-        </div>
-        <div id='widget_sick' class='widget_emoji'>
-          <i class='twa twa-5x twa-face-with-thermometer'></i>
-          Sick
-        </div>
-        <div id='widget_tired' class='widget_emoji'>
-          <i class='twa twa-5x twa-sleeping-face'></i>
-          Tired
-          </div>
-        </div>
-        <div class='widget_emoji_container emoji_row'>
-            <div id='widget_speak_up' class='widget_emoji'>
-                <i class='twa twa-5x twa-eye-in-speech-bubble'></i>
-                Speak Up
-            </div>
-            <div id='widget_needs_help' class='widget_emoji'>
-            <i class='twa twa-5x twa-sos-button'></i>
-            I need help
-            </div>
-            <div id='widget_needs_talk' class='widget_emoji'>
-            <i class='twa twa-5x twa-speech-balloon'></i>
-            I need to talk
-            </div>
-        </div>
-       </div>";
+      </div>";
 //echo "<div class='widget_container widget_body' style='color:#666;'>Menu<i class='right material-icons widget_holder_refresh pointer' data-path='/modules/Abre-Moods/widget_menu_or_roster.php' data-reload='true'>menu</i></div>";
 //echo "<div class='widget_container widget_body' style='color:#666;'>History<i class='right material-icons widget_holder_refresh pointer' data-path='/modules/Abre-Moods/widget_history_or_overview.php' data-reload='true'>history</i></div>";
 //echo "</div>";
