ffd460b41c5be8dd33adf663978f6a2435716604
Josh <Josh.christensen48@masonohioschools.com>
2019-07-01 16:45:33 -0400

    just cuz macs and pc dont like to share

--- modules/Abre-Moods/fullscreen/teachers/overview.php
+++ modules/Abre-Moods/fullscreen/teachers/overview.php
@@ -7,6 +7,17 @@ echo "<link rel='stylesheet' type='text/css' href='/modules/" . basename(dirname
     <div id="class_head_bar" class="zone_head_bar">
         Class Name <span class="bell"> Bell #</span>
         <i class="material-icons pick_bell">expand_more</i>
+
+        <!-- <div class="input-field col s12">
+            <select>
+              <option value="" disabled selected>Choose your option</option>
+              <option value="1">Option 1</option>
+              <option value="2">Option 2</option>
+              <option value="3">Option 3</option>
+            </select>
+            <label>Materialize Select</label>
+          </div> -->
+
         <span class="num_students">##/##</span>
         <div class="loading_bar class_progress_bar"></div>
     </div>
