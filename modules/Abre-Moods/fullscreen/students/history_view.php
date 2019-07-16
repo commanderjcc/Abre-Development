7f06adf405e98c8717adbdb04bf321ca4223162d
Josh <josh.christensen48@masonohioschools.com>
2019-07-16 11:53:21 -0400

    Computer Layout done

--- modules/Abre-Moods/fullscreen/students/history_view.php
+++ modules/Abre-Moods/fullscreen/students/history_view.php
@@ -1,4 +1,31 @@
 <?php
 
 require_once(dirname(__FILE__) . '/../../../../core/abre_verification.php');
-echo "history view";
+echo "<link rel='stylesheet' type='text/css' href='/modules/" . basename(dirname(__DIR__, 2)) . "/css/student/history.css'>";
+?>
+<div id="historyPage" class="">
+    <div id="informationPane" class="">
+        <div id="currentMoodInfo" class="mdl-shadow--2dp detail-container mcontainer">
+            <div style="background-image: url('/profile.jpg')" id="profileHolder" class="student_image">
+                <i class="student_mood twa twa-3x twa-sad"></i>
+            </div>
+            <div id="studentDetails" class="">
+                <span id="student_name" class="">Joshua Christensen</span>
+                <span id="lastMood" class="">"Frustrated" at 12:12 AM</span>
+            </div>
+        </div>
+        <div id="moodHistory" class="mdl-shadow--2dp detail-container mcontainer">
+            <div class="mood">
+                <i class="twa twa-3x twa-sad"></i>
+                <div class="moodDetails">
+                    <span class="moodName">"Frustrated" <span class="moodTime">12:12 AM</span></span>
+                    <span class="moodDate">Monday, Jan 12th</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="graphHolder" class="mdl-card mdl-shadow--4dp mcontainer">
+
+    </div>
+
+</div>
