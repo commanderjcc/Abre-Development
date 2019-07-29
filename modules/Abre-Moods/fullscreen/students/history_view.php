e7ed84a3a2e44edae0b12978d35b2cbbf9901aae
Josh <josh.christensen48@masonohioschools.com>
2019-07-29 16:29:38 -0400

    Show data on the left column

--- modules/Abre-Moods/fullscreen/students/history_view.php
+++ modules/Abre-Moods/fullscreen/students/history_view.php
@@ -7,7 +7,7 @@ echo "<link rel='stylesheet' type='text/css' href='/modules/" . basename(dirname
     <div id="informationPane" class="">
         <div id="currentMoodInfo" class="mdl-shadow--2dp detail-container mcontainer">
             <div style="background-image: url('/profile.jpg')" id="profileHolder" class="student_image">
-                <i class="student_mood twa twa-3x twa-sad"></i>
+                <i class="student_mood twa twa-3x"></i>
             </div>
             <div id="studentDetails" class="">
                 <span id="student_name" class="">Joshua Christensen</span>
@@ -15,13 +15,6 @@ echo "<link rel='stylesheet' type='text/css' href='/modules/" . basename(dirname
             </div>
         </div>
         <div id="moodHistory" class="mdl-shadow--2dp detail-container mcontainer">
-            <div class="mood">
-                <i class="twa twa-3x twa-sad"></i>
-                <div class="moodDetails">
-                    <span class="moodName">"Frustrated" <span class="moodTime">12:12 AM</span></span>
-                    <span class="moodDate">Monday, Jan 12th</span>
-                </div>
-            </div>
         </div>
     </div>
     <div id="graphHolder" class="mdl-shadow--4dp mcontainer">
@@ -34,117 +27,81 @@ echo "<link rel='stylesheet' type='text/css' href='/modules/" . basename(dirname
 
 </div>
 <script defer type="text/javascript">
+    var capitalizeFirst = function(str) {
+        return str.replace(/\w\S*/g,function(txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        })
+    }
+    var zoneConversion = ['', 'Blue', 'Green', 'Yellow', 'Red', ''];
+    var jqxhr = $.post('/modules/Abre-Moods/Data_Access/Students/get_history.php', {studentID:<?php echo intval($_POST['studentID']); ?>});
+    var chart;
+    jqxhr.done(function (data) {
+        console.log(JSON.parse(data));
+        data = JSON.parse(data);
+        var graphData = {
 
-    var jqxhr = $.post('/modules/Abre-Moods/Data_Access/Students/get_history.php',{studentID:<?php echo intval($_POST['studentID']); ?>});
-    jqxhr.done(function(data){
-        console.log(JSON.parse(data))
-    });
-    var data = {
-
-
-        // Our series array that contains series objects or in this case series data arrays
-        series: [
-            {
-                name: 'mood',
-                data: [
-                    {x: new moment().year(2018).month(11).day(1).valueOf(), y: 4},
-                    {x: new moment().year(2019).month(0).day(1).valueOf(), y: 2},
-                    // {x: new moment().year(2018).month(12).day(31).valueOf(), y: 3},
-                    {x: new moment().year(2019).month(1).day(1).valueOf(), y: 2},
-                    {x: new moment().year(2019).month(1).day(2).valueOf(), y: 2},
-                    {x: new moment().year(2019).month(1).day(3).valueOf(), y: 2},
-                    {x: new moment(1555555555555).valueOf(), y: 2},
-                    // {x: new moment().year(2019).month(1).day(31).valueOf(), y: 1},
-                    {x: new moment().year(2019).month(2).day(1).valueOf(), y: 4},
-                    // {x: new moment().year(2019).month(2).day(28).valueOf(), y: 4},
-                    {x: new moment().year(2019).month(3).day(1).valueOf(), y: 4},
-                    // {x: new moment().year(2019).month(3).day(31).valueOf(), y: 4},
-                    {x: new moment().year(2019).month(4).day(1).valueOf(), y: 4},
-                    // {x: new moment().year(2019).month(4).day(30).valueOf(), y: 4},
-                    {x: new moment().year(2019).month(5).day(1).valueOf(), y: 4},
-                    // {x: new moment().year(2019).month(5).day(31).valueOf(), y: 4},
-                    {x: new moment().year(2019).month(6).day(1).valueOf(), y: 4},
-                    // {x: new moment().year(2019).month(6).day(30).valueOf(), y: 4},
-                    {x: new moment().year(2019).month(7).day(1).valueOf(), y: 4},
-                    // {x: new moment().year(2019).month(7).day(31).valueOf(), y: 4},
-                    {x: new moment().year(2019).month(8).day(1).valueOf(), y: 4},
-                    // {x: new moment().year(2019).month(8).day(30).valueOf(), y: 4},
-                    {x: new moment().year(2019).month(9).day(1).valueOf(), y: 4},
-                    // {x: new moment().year(2019).month(9).day(31).valueOf(), y: 4},
-                    {x: new moment().year(2019).month(10).day(1).valueOf(), y: 4},
-                    // {x: new moment().year(2019).month(10).day(31).valueOf(), y: 4},
-                    {x: new moment().year(2019).month(11).day(1).valueOf(), y: 4},
-                    // {x: new moment().year(2019).month(11).day(30).valueOf(), y: 4},
-                    {x: new moment().year(2020).month(0).day(1).valueOf(), y: 4},
-                    {x: new moment().year(2019).month(12).day(31).valueOf(), y: 4},
-
-                ]
-            }
-        ]
-    };
-
-    var zoneConversion = ['','Blue','Green','Yellow','Red',''];
-
-    // As options we currently only set a static size of 300x200 px
-    var options = {
-        // high: new Date(2019, 11, 31),
-        // low: new Date(2019,0,1),
-        // width: '300px',
-        // height: '200px',
-        onlyInteger: true,
-        axisY: {
-
-            type: Chartist.FixedScaleAxis,
-            high: 5,
-            low: 0,
-            divisor: 5,
-            showGrid: false,
-            referenceValue: 1,
-            labelInterpolationFnc: function(value) {
-                return zoneConversion[value];
-            },
-        },
-        axisX: {
-            type: Chartist.FixedScaleAxis,
-            high: new moment('20191231').valueOf(),
-            low: new moment('20190101').valueOf(),
-            // high: new moment().year(2019).month(11).day(31).valueOf(),
-            // low: new moment().year(2019).month(0).day(1).valueOf(),
-            // referenceValue: new moment().year(2019).month(0).day(1).valueOf(),
-            // divisor: 13,
-            ticks: [
-                new moment('20190101').valueOf(),
-                new moment('20190201').valueOf(),
-                new moment('20190301').valueOf(),
-                new moment('20190401').valueOf(),
-                new moment('20190501').valueOf(),
-                new moment('20190601').valueOf(),
-                new moment('20190701').valueOf(),
-                new moment('20190801').valueOf(),
-                new moment('20190901').valueOf(),
-                new moment('20191001').valueOf(),
-                new moment('20191101').valueOf(),
-                new moment('20191201').valueOf(),
-            ],
-            labelInterpolationFnc: function(value) {
-                console.log(value);
-                return moment(value).format('MMM D YY');
-            }
-        },
-        plugins: [
-            Chartist.plugins.tooltip({
-                anchorToPoint: true,
-                appendToBody: true,
-                transformTooltipTextFnc: function(value) {
-                    console.log(value);
-                    return new moment(parseInt(value.split(',')[0])).format('MMM D');
+            // Our series array that contains series objects or in this case series data arrays
+            series: [
+                {
+                    name: 'mood',
+                    data: data.history.map((moodObj)=>{
+                        console.log(moodObj);
+                        return {'meta':moodObj.mood,'x':moment(moodObj.time).valueOf(),'y':zoneConversion.indexOf(capitalizeFirst(moodObj.zone))}
+                    })
                 }
-            }),
-        ],
-    };
+            ]
+        };
 
-    // In the global name space Chartist we call the Line function to initialize a line chart. As a first parameter we pass in a selector where we would like to get our chart created. Second parameter is the actual data object and as a third parameter we pass in our options
-    var chart = new Chartist.Line('.ct-chart', data, options);
+        var options = {
+            onlyInteger: true,
+            axisY: {
+
+                type: Chartist.FixedScaleAxis,
+                high: 5,
+                low: 0,
+                divisor: 5,
+                referenceValue: 1,
+                labelInterpolationFnc: function (value) {
+                    return zoneConversion[value];
+                },
+            },
+            axisX: {
+                type: Chartist.FixedScaleAxis,
+                high: new moment('20191231').valueOf(),
+                low: new moment('20190101').valueOf(),
+                ticks: [
+                    new moment('20190101').valueOf(),
+                    new moment('20190201').valueOf(),
+                    new moment('20190301').valueOf(),
+                    new moment('20190401').valueOf(),
+                    new moment('20190501').valueOf(),
+                    new moment('20190601').valueOf(),
+                    new moment('20190701').valueOf(),
+                    new moment('20190801').valueOf(),
+                    new moment('20190901').valueOf(),
+                    new moment('20191001').valueOf(),
+                    new moment('20191101').valueOf(),
+                    new moment('20191201').valueOf(),
+                ],
+                labelInterpolationFnc: function (value) {
+                    console.log(value);
+                    return moment(value).format('MMM D YY');
+                }
+            },
+            plugins: [
+                Chartist.plugins.tooltip({
+                    anchorToPoint: true,
+                    appendToBody: true,
+                    transformTooltipTextFnc: function (value) {
+                        console.log(value);
+                        return new moment(parseInt(value.split(',')[0])).format('MMM D');
+                    }
+                }),
+            ],
+        };
+
+        // In the global name space Chartist we call the Line function to initialize a line chart. As a first parameter we pass in a selector where we would like to get our chart created. Second parameter is the actual data object and as a third parameter we pass in our options
+        chart = new Chartist.Line('.ct-chart', graphData, options);
         chart.on('draw', function (data) {
             if (data.type === 'grid') {
 
@@ -168,4 +125,23 @@ echo "<link rel='stylesheet' type='text/css' href='/modules/" . basename(dirname
                 }
             }
         });
+
+        $('#student_name').text(data.name);
+        $('#profileHolder').css('background-image','url("'+data.photo+'")');
+        $('#profileHolder .student_mood').addClass('twa-'+data.mood);
+        $('#lastMood').text('"'+capitalizeFirst(data.mood)+'" at '+moment(data.time).format('h:mm A'));
+
+        var outline = [`<div class="mood">
+                <i class="twa twa-3x twa-`,`"></i>
+                <div class="moodDetails">
+                    <span class="moodName">"`,`" <span class="moodTime">`,`</span></span>
+                    <span class="moodDate">`,`</span>
+                </div>
+            </div>`];
+        data.history.forEach((moodObj)=>{
+            $('#moodHistory').append(outline[0]+data.mood+outline[1]+data.name+outline[2]+moment(data.time).format('h:mm A')+outline[3]+moment(data.time).format('dddd[,] MMM Do'))
+        })
+
+    });
+
 </script>
